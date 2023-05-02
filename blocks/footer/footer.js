1410cf018996cd92bca43feb2f4d9535c01ee808
Borrowman <scobor01@10250-ap-00005.local>
2023-05-02 07:45:44 -0600

    Adding styles

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,25 +1,33 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { readBlockConfig} from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
-  block.textContent = '';
+	const cfg = readBlockConfig(block);
+	block.textContent = '';
 
-  // fetch footer content
-  const footerPath = cfg.footer || '/footer';
-  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+	// fetch footer content
+	const footerPath = cfg.footer || '/footer';
+	const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
-  if (resp.ok) {
-    const html = await resp.text();
+	if (resp.ok) {
+		const html = await resp.text();
 
-    // decorate footer DOM
-    const footer = document.createElement('div');
-    footer.innerHTML = html;
+		// decorate footer DOM
+		const footer = document.createElement('div');
+		footer.innerHTML = html;
 
-    decorateIcons(footer);
-    block.append(footer);
-  }
+		block.append(footer);
+
+		const footerGrid = footer.querySelector('.column-grid > div');
+		const footerColumns = Array.from(footerGrid.children)
+		
+		footerColumns.forEach((column, index) => {
+			column.classList.add('column');
+
+			if (index === 1) column.classList.add('footer-links');
+		});
+	}
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,16 @@
 import {
-  sampleRUM,
-  buildBlock,
-  loadHeader,
-  loadFooter,
-  decorateButtons,
-  decorateIcons,
-  decorateSections,
-  decorateBlocks,
-  decorateTemplateAndTheme,
-  waitForLCP,
-  loadBlocks,
-  loadCSS,
+	sampleRUM,
+	buildBlock,
+	loadHeader,
+	loadFooter,
+	decorateButtons,
+	decorateIcons,
+	decorateSections,
+	decorateBlocks,
+	decorateTemplateAndTheme,
+	waitForLCP,
+	loadBlocks,
+	loadCSS,
 } from './lib-franklin.js';
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
@@ -20,14 +20,14 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
  * @param {Element} main The container element
  */
 function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
-  }
+	const h1 = main.querySelector('h1');
+	const picture = main.querySelector('picture');
+	// eslint-disable-next-line no-bitwise
+	if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+		const section = document.createElement('div');
+		section.append(buildBlock('hero', { elems: [picture, h1] }));
+		main.prepend(section);
+	}
 }
 
 /**
@@ -35,12 +35,12 @@ function buildHeroBlock(main) {
  * @param {Element} main The container element
  */
 function buildAutoBlocks(main) {
-  try {
-    buildHeroBlock(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
-  }
+	try {
+		buildHeroBlock(main);
+	} catch (error) {
+		// eslint-disable-next-line no-console
+		console.error('Auto Blocking failed', error);
+	}
 }
 
 /**
@@ -49,12 +49,12 @@ function buildAutoBlocks(main) {
  */
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main) {
-  // hopefully forward compatible button decoration
-  decorateButtons(main);
-  decorateIcons(main);
-  buildAutoBlocks(main);
-  decorateSections(main);
-  decorateBlocks(main);
+	// hopefully forward compatible button decoration
+	decorateButtons(main);
+	decorateIcons(main);
+	buildAutoBlocks(main);
+	decorateSections(main);
+	decorateBlocks(main);
 }
 
 /**
@@ -62,14 +62,14 @@ export function decorateMain(main) {
  * @param {Element} doc The container element
  */
 async function loadEager(doc) {
-  document.documentElement.lang = 'en';
-  decorateTemplateAndTheme();
-  const main = doc.querySelector('main');
-  if (main) {
-    decorateMain(main);
-    document.body.classList.add('appear');
-    await waitForLCP(LCP_BLOCKS);
-  }
+	document.documentElement.lang = 'en';
+	decorateTemplateAndTheme();
+	const main = doc.querySelector('main');
+	if (main) {
+		decorateMain(main);
+		document.body.classList.add('appear');
+		await waitForLCP(LCP_BLOCKS);
+	}
 }
 
 /**
@@ -77,16 +77,16 @@ async function loadEager(doc) {
  * @param {string} href The favicon URL
  */
 export function addFavIcon(href) {
-  const link = document.createElement('link');
-  link.rel = 'icon';
-  link.type = 'image/svg+xml';
-  link.href = href;
-  const existingLink = document.querySelector('head link[rel="icon"]');
-  if (existingLink) {
-    existingLink.parentElement.replaceChild(link, existingLink);
-  } else {
-    document.getElementsByTagName('head')[0].appendChild(link);
-  }
+	const link = document.createElement('link');
+	link.rel = 'icon';
+	link.type = 'image/svg+xml';
+	link.href = href;
+	const existingLink = document.querySelector('head link[rel="icon"]');
+	if (existingLink) {
+		existingLink.parentElement.replaceChild(link, existingLink);
+	} else {
+		document.getElementsByTagName('head')[0].appendChild(link);
+	}
 }
 
 /**
@@ -94,21 +94,21 @@ export function addFavIcon(href) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  const main = doc.querySelector('main');
-  await loadBlocks(main);
+	const main = doc.querySelector('main');
+	await loadBlocks(main);
 
-  const { hash } = window.location;
-  const element = hash ? doc.getElementById(hash.substring(1)) : false;
-  if (hash && element) element.scrollIntoView();
+	const { hash } = window.location;
+	const element = hash ? doc.getElementById(hash.substring(1)) : false;
+	if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
+	loadHeader(doc.querySelector('header'));
+	loadFooter(doc.querySelector('footer'));
 
-  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
-  sampleRUM('lazy');
-  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
-  sampleRUM.observe(main.querySelectorAll('picture > img'));
+	loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+	addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
+	sampleRUM('lazy');
+	sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
+	sampleRUM.observe(main.querySelectorAll('picture > img'));
 }
 
 /**
@@ -116,15 +116,15 @@ async function loadLazy(doc) {
  * without impacting the user experience.
  */
 function loadDelayed() {
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
-  // load anything that can be postponed to the latest here
+	// eslint-disable-next-line import/no-cycle
+	window.setTimeout(() => import('./delayed.js'), 3000);
+	// load anything that can be postponed to the latest here
 }
 
 async function loadPage() {
-  await loadEager(document);
-  await loadLazy(document);
-  loadDelayed();
+	await loadEager(document);
+	await loadLazy(document);
+	loadDelayed();
 }
 
 loadPage();
